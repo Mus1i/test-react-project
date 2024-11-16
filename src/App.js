073a4e9324e52eacba0715bf7ef3d7979bcbba03
Mus1i <v_kazin@mail.ru>
2024-11-16 14:39:57 +0300

    App.js with createElement() from React.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { createElement } from "react";
 
 function App() {
 	const currentYear = new Date().getFullYear();
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<footer>{currentYear}</footer>
-			</header>
-		</div>
+	const logoElement = createElement("img", {
+		src: logo,
+		className: "App-logo",
+		alt: "logo",
+	});
+
+	const editParagraph = createElement(
+		"p",
+		null,
+		"Edit ",
+		createElement("code", null, "src/App.js"),
+		" and save to reload."
 	);
+
+	const linkElement = createElement(
+		"a",
+		{
+			className: "App-link",
+			href: "https://reactjs.org",
+			target: "_blank",
+			rel: "noopener noreferrer",
+		},
+		"Learn React"
+	);
+
+	const yearHeader = createElement("h1", null, currentYear);
+
+	const headerElement = createElement(
+		"header",
+		{ className: "App-header" },
+		logoElement,
+		editParagraph,
+		linkElement,
+		yearHeader
+	);
+
+	return createElement("div", { className: "App" }, headerElement);
 }
 
 export default App;
